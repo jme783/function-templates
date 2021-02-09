d5fdf201c03dfaf588ccd4b620b172ed66a1f4fd
Jeff Eiden <jeff.m.eiden@gmail.com>
2021-02-09 12:58:06 -0800

    Create new version of save resident funciton with Google Sheets API

--- vaccine-standby-list/functions/save-resident.protected.js
+++ vaccine-standby-list/functions/save-resident.protected.js
@@ -1,7 +1,7 @@
-exports.handler = function(context, event, callback) {
-    var Airtable = require('airtable');
+exports.handler = async function(context, event, callback) {
+    /*var Airtable = require('airtable');
     var base = new Airtable({apiKey: context.AIRTABLE_API_KEY}).base(context.AIRTABLE_BASE_ID);
-
+    
     base('Registered Residents').create([
         {
             "fields": {
@@ -26,5 +26,38 @@ exports.handler = function(context, event, callback) {
         }
 
         callback(null, 'success');
+    });*/
+
+    // Google Sheets version
+    const { GoogleSpreadsheet } = require('google-spreadsheet');
+    const sheet = new GoogleSpreadsheet(context.GOOGLE_SHEET_ID);
+
+    await sheet.useServiceAccountAuth({
+        client_email: context.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: context.GOOGLE_PRIVATE_KEY,
     });
+
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+    const rows = await sheet.getRows();
+    try {
+        await sheet.addRow({
+            "Name": event.name,
+            "Phone Number": event.phone,
+            "Email": event.email,
+            "Age": event.age,
+            "Essential Worker": event.essential_worker,
+            "Zip Code": event.zip_code,
+            "Work From Home": event.work_from_home,
+            "Live in Long Term Care Facility": event.long_term_care,
+            "Live in Congregate Setting": event.congregate_setting,
+            "Underlying Health Condition": event.underlying_health_condition,
+            "Notification Preference": event.notification_preference,
+            "Language Preference": event.language_preference,
+        });
+        callback(null, 'success');    
+    } catch(err) {
+        callback('Error writing to Google Sheets', null);
+    }
+
 };

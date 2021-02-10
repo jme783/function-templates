39cbf4d654799ce3d190f583b26364c195fd0c31
Jeff Eiden <jeff.m.eiden@gmail.com>
2021-02-10 13:26:58 -0800

    vaccine-standby-list --> vaccine-standby

--- vaccine-standby/functions/return-config.js
+++ vaccine-standby/functions/return-config.js
@@ -1,9 +1,9 @@
 exports.handler = function(context, event, callback) {
-  let airtable_embed_code = context.AIRTABLE_EMBED_CODE;
+  let google_sheet_id = context.GOOGLE_SHEET_ID;
   let phone_number = context.TWILIO_PHONE_NUMBER;
   let response = new Twilio.Response();
   response.setStatusCode(200);
   response.appendHeader('Content-Type', 'application/json');
-  response.setBody({'airtable_embed_code': airtable_embed_code, 'phone_number': phone_number});
+  response.setBody({'google_sheet_id': google_sheet_id, 'phone_number': phone_number});
   callback(null, response);
 };
